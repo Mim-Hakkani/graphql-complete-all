194cae38f58e3cb91fb2009a42b5ae2fcf52ef91
32-Mimhakkani <mim.cse32@gmail.com>
2022-03-19 11:55:16 +0600

    settp express server and graphql

--- backent/index.js
+++ backent/index.js
@@ -1,6 +1,37 @@
 const express = require('express')
+const { graphqlHTTP } = require('express-graphql');
 const app = express()
 const port = 8000
+
+
+// var { buildSchema } = require('graphql');
+
+// // Construct a schema, using GraphQL schema language
+// var schema = buildSchema(`
+//   type Query {
+//     hello: String,
+//     name:String,
+//     id:String
+//   }
+// `);
+
+// // The root provides a resolver function for each API endpoint
+// var root = {
+//   hello: () => {
+//     return 'Hello world!';
+//   },
+//   name:()=>{
+//       return 'golam hkkani mim'
+//   }
+// };
+
+
+app.use('/graphql', graphqlHTTP({
+//   schema: schema,
+//   rootValue: root,
+//   graphiql: true,
+}));
+
 
 app.get('/', (req, res) => {
   res.send('Graphql hellow world !!')
