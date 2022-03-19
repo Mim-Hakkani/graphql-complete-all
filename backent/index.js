783eb281b3388ee4879aab53bcc215f531a734f5
32-Mimhakkani <mim.cse32@gmail.com>
2022-03-19 15:02:08 +0600

    connect to mongodb

--- backent/index.js
+++ backent/index.js
@@ -1,30 +1,24 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql');
 const Schema = require('./Schema/Schema');
+const mongoose = require("mongoose");
+const cors = require("cors");
+
 const app = express()
 const port = 8000
 
+app.use(cors());
 
-// var { buildSchema } = require('graphql');
+//connect to mongodb
 
-// // Construct a schema, using GraphQL schema language
-// var schema = buildSchema(`
-//   type Query {
-//     hello: String,
-//     name:String,
-//     id:String
-//   }
-// `);
-
-// // The root provides a resolver function for each API endpoint
-// var root = {
-//   hello: () => {
-//     return 'Hello world!';
-//   },
-//   name:()=>{
-//       return 'golam hkkani mim'
-//   }
-// };
+mongoose.connect(
+  `mongodb+srv://graphQLPracticeUser:j71p7z32Rfzb2Ris@cluster0.0om44.mongodb.net/graphQLPractice?retryWrites=true&w=majority
+  
+  `
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to DB by mim hakkani");
+});
 
 
 app.use('/graphql', graphqlHTTP({
